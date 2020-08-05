8adbef756a6ee709b0a5a89d302deb68d38011fd
nbur4556 <nbur4556@gmail.com>
2020-08-05 14:35:53 -0500

    random word chosen from available words

--- assets/scripts/gameplay.js
+++ assets/scripts/gameplay.js
@@ -1,4 +1,10 @@
 // Player guess mode
+var availableWords = allKnownWords;
+
+function pickRandomWord() {
+    var wordIndex = Math.floor(Math.random() * availableWords.length) + 1;
+    return availableWords[wordIndex];
+}
 
 // AI guess mode
 
