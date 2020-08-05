37aa5dc936449b6bbd24f475421024b7ecf27505
nbur4556 <nbur4556@gmail.com>
2020-08-05 15:08:36 -0500

    created function for guessing letter and checking if matches with game word

--- assets/scripts/gameplay.js
+++ assets/scripts/gameplay.js
@@ -1,9 +1,40 @@
-// Player guess mode
-var availableWords = allKnownWords;
+// Elements
+var letterBtns = document.querySelectorAll('.letter');
 
+for (let i = 0; i < letterBtns.length; i++) {
+    letterBtns[i].addEventListener('click', guessLetter);
+}
+
+//Variables
+let availableWords = allKnownWords;
+let gameWord = pickRandomWord();
+
+// Word is chosen randomly ftom available word array
 function pickRandomWord() {
-    var wordIndex = Math.floor(Math.random() * availableWords.length) + 1;
+    let wordIndex = Math.floor(Math.random() * availableWords.length) + 1;
     return availableWords[wordIndex];
+}
+
+//Check if guessed letter is in gameWord
+function guessLetter(e) {
+    let guessLetter = e.target.textContent;
+    let guessTrue = false;
+
+    for (let i = 0; i < gameWord.length; i++) {
+        if (guessLetter.toLowerCase() == gameWord[i].toLowerCase()) {
+            guessTrue = true;
+        }
+    }
+
+    if (guessTrue) {
+        //RUN IF MATCH
+    }
+    else {
+        //RUN IF NO MATCH
+    }
+
+    // Disable button for guessed letter
+    e.target.disabled = true;
 }
 
 // AI guess mode
